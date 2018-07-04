b5ee45ab75c8f38aa7849acd361a9c467e682773
CaiHuan <cai.huan25@gmail.com>
2018-07-04 07:14:56 -0700

    onekey run debug_server

--- lldb_server/push_start_debug_server.sh
+++ lldb_server/push_start_debug_server.sh
@@ -4,4 +4,5 @@ LLDB_DIR=/data/local/tmp/lldb_server
 
 adb push bin $LLDB_DIR/bin/
 adb push start_lldb_server.sh $LLDB_DIR/start_lldb_server.sh
-adb shell chmod 0775 $LLDB_DIR/start_lldb_server.sh
+adb shell "su 0 chmod 777 $LLDB_DIR/start_lldb_server.sh"
+adb shell "su 0 $LLDB_DIR/start_lldb_server.sh"
